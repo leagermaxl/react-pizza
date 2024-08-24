052a3acd1277dec44a6c6e84a6477749ee69f806
Max Lyubchenko <00eagermax00@gmail.com>
2024-08-24 18:25:25 +0300

    Fix bug with PizzaBlock.

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import style from './PizzaBlock.module.scss';
 
 function PizzaBlock({ title, price, imageUrl, sizes, types }) {
-  const [activeType, setActiveType] = React.useState(0);
-  const [activeSize, setActiveSize] = React.useState(0);
+  const [activeType, setActiveType] = React.useState(types[0]);
+  const [activeSize, setActiveSize] = React.useState(sizes[0]);
 
-  const typeNames = ['тонкое', 'традиционное'];
+  const typeArray = ['тонкое', 'традиционное'];
+  const sizeArray = [26, 30, 40];
 
   return (
     <div className={style.pizzaBlockWrapper}>
@@ -15,28 +16,36 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
         <h2>{title}</h2>
         <div className={style.options}>
           <ul>
-            {types.map((typeId) => {
+            {typeArray.map((typeName, index) => {
+              const typeIncludes = types.includes(index);
               return (
                 <li
-                  key={typeId}
-                  onClick={() => setActiveType(typeId)}
-                  className={`${activeType === typeId ? style.active : ''}`}
+                  key={index}
+                  onClick={typeIncludes ? () => setActiveType(index) : () => {}}
+                  className={`${activeType === index ? style.active : ''} ${
+                    typeIncludes ? '' : style.disabled
+                  }`}
                 >
-                  {typeNames[typeId]}
+                  {typeName}
                 </li>
               );
             })}
           </ul>
           <ul>
-            {sizes.map((size, index) => (
-              <li
-                key={index}
-                onClick={() => setActiveSize(index)}
-                className={activeSize === index ? style.active : ''}
-              >
-                {size} см.
-              </li>
-            ))}
+            {sizeArray.map((size) => {
+              const sizeIncludes = sizes.includes(size);
+              return (
+                <li
+                  key={size}
+                  onClick={sizeIncludes ? () => setActiveSize(size) : () => {}}
+                  className={`${activeSize === size ? style.active : ''} ${
+                    sizeIncludes ? '' : style.disabled
+                  }`}
+                >
+                  {size} см.
+                </li>
+              );
+            })}
           </ul>
         </div>
         <div className={style.pizzaBlockInfo}>
