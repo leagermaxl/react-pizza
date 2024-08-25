a16302518cb84d094d4e11774e14a4609e1cadc6
Max Lyubchenko <00eagermax00@gmail.com>
2024-08-25 18:29:55 +0300

    Create new pizzaSlice.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filterSlice from './slices/filterSlice';
 import cartSlice from './slices/cartSlice';
+import pizzaSlice from './slices/pizzaSlice';
 
 export default configureStore({
-  reducer: { filterSlice, cartSlice },
+  reducer: { filterSlice, cartSlice, pizzaSlice },
 });
