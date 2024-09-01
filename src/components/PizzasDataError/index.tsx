e5e513e84a158be62c1407d63daef385bac9ef41
Max Lyubchenko <00eagermax00@gmail.com>
2024-09-01 22:48:25 +0300

    Replaced .jsx types to .tsx in PizzaDataError.

--- src/components/PizzasDataError/index.tsx
+++ src/components/PizzasDataError/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import styles from './PizzasDataError.module.scss';
 
-export default function PizzasDataError() {
+const PizzasDataError: React.FC = () => {
   return (
     <div className={styles.root}>
       <h2>Произошла ошибка 😕</h2>
       <p>К сожалению, не удалось получить пиццы. Попробуйте повторить попытку позже.</p>
     </div>
   );
-}
+};
+export default PizzasDataError;
