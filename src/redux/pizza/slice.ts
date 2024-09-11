69ca4d241971e44a75ab73d6cc3f5240a3c10de4
Max Lyubchenko <00eagermax00@gmail.com>
2024-09-11 22:30:40 +0300

    Added in Redux pizza processing request one pizza by id.

--- src/redux/pizza/slice.ts
+++ src/redux/pizza/slice.ts
@@ -1,10 +1,30 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import { Pizza, PizzaSliceState, Status } from './types';
-import { fetchDataPizzas } from './asyncActions';
+import { fetchDataPizzas, fetchFullPizza } from './asyncActions';
+
+const emptyPizza = {
+  id: 0,
+  title: '',
+  price: [0],
+  imageUrl: '',
+  sizes: [],
+  types: [],
+  category: 0,
+  rating: 0,
+  composition: '',
+  weights: [0],
+  nutritionalValue: {
+    energyValue: 0,
+    protein: 0,
+    fats: 0,
+    carbohydrates: 0,
+  },
+};
 
 const initialState: PizzaSliceState = {
   itemsPizzas: [],
+  itemPizza: emptyPizza,
   status: Status.LOADING, // loading, success, error
 };
 
@@ -31,6 +51,18 @@ const pizzaSlice = createSlice({
       })
       .addCase(fetchDataPizzas.rejected, (state) => {
         state.itemsPizzas = [];
+        state.status = Status.ERROR;
+      })
+      .addCase(fetchFullPizza.pending, (state) => {
+        state.itemPizza = emptyPizza;
+        state.status = Status.LOADING;
+      })
+      .addCase(fetchFullPizza.fulfilled, (state, action: PayloadAction<Pizza>) => {
+        state.itemPizza = action.payload;
+        state.status = Status.SUCCESS;
+      })
+      .addCase(fetchFullPizza.rejected, (state) => {
+        state.itemPizza = emptyPizza;
         state.status = Status.ERROR;
       });
   },
